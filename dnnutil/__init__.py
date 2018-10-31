dc747f0f0425a9b5a8db3b1145ae56b686b1b039
Connor <catalys1@gmail.com>
2018-10-31 10:59:05 -0600

    Add stubs for configuration files

--- dnnutil/__init__.py
+++ dnnutil/__init__.py
@@ -1,7 +1,8 @@
 from .cmdopts import basic_parser
 from .logger import TextLog
 from .network import tocuda, load_model, save_model
-from .training import Trainer, ClassifierTrainer, AutoencoderTrainer, calculate_accuracy
+from .training import (Trainer, ClassifierTrainer, AutoencoderTrainer,
+                       calculate_accuracy)
 from .manage import Checkpointer, Manager
 from .optim import EpochSetLR
 
@@ -11,3 +12,4 @@ IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD = [0.229, 0.224, 0.225]
 
 template = __path__[0] + '/template.txt'
+config = __path__[0] + '/config.json'
--- dnnutil/__main__.py
+++ dnnutil/__main__.py
@@ -1,13 +1,17 @@
 import dnnutil
+import os
+from pathlib import Path
 
 
 def make_template(args):
-    import os
     if os.path.isfile(args.file):
         while True:
-            resp = input(f'{args.file} already exists. Overwrite it [y|N]? ')
+            resp = input(
+                '{} already exists. Overwrite it [y|N]? '.format(args.file))
             if resp.lower() in ['y', 'n']:
                 break
+            elif resp == '':
+                resp = 'n'
         if resp == 'n':
             return
 
@@ -17,14 +21,41 @@ def make_template(args):
         f.write(temp)
 
 
+def make_config(args):
+    conf = Path(args.file).with_suffix('.json')
+    if conf.is_file():
+        while True:
+            resp = input(
+                '{} already exists. Overwrite it [y|N]? '.format(str(conf)))
+            if resp.lower() in ['y', 'n']:
+                break
+            elif resp == '':
+                resp = 'n'
+        if resp == 'n':
+            return
+
+    with open(dnnutil.config, 'r') as f:
+        temp = f.read()
+    with conf.open('w') as f:
+        f.write(temp)
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     subs = parser.add_subparsers()
+
     parse_new = subs.add_parser('new', aliases=['n'])
     parse_new.add_argument('-f', '--file', type=str, default='main.py',
         help='Name (location) of output file. Default: ./main.py')
     parse_new.set_defaults(func=make_template)
+
+    parse_conf = subs.add_parser('conf', aliases=['c'])
+    parse_conf.add_argument('file', type=str,
+        help='Name (location) of the output file. The extension should be '
+             '.json. If no extension is given, .json will be appended.')
+    parse_conf.set_defaults(func=make_config)
+
     args = parser.parse_args()
 
     if 'func' in args:
