122d3e0b0551db3881ec9128288e8635f53dbc3d
Connor <catalys1@gmail.com>
2018-11-03 08:20:09 -0600

    Add support for config files. Add a parser for configs.

--- dnnutil/cmdopts.py
+++ dnnutil/cmdopts.py
@@ -2,6 +2,9 @@ import argparse
 from pathlib import Path
 
 
+__all__ = ['basic_parser', 'config_parser']
+
+
 def basic_parser(description='', **kwargs):
     '''Returns an argparse.ArgumentParser commandline parser, pre-populated
     with common arguments used when training deep networks. The argument
@@ -39,7 +42,7 @@ def basic_parser(description='', **kwargs):
     a = parser.add_argument('--test', action='store_true', default=False,
         help='Runs inference on the model.')
     a = parser.add_argument('--rid', type=int, default=0,
-        help='Positive int representing run id. The run id can be used to keep '
+        help='Integer representing run id. The run id can be used to keep '
              'seperate logs and checkpoints. Default: 0')
     a = parser.add_argument('--note', default='',
         help='Any notes about the model or run. Can be used to keep track of '
@@ -50,3 +53,32 @@ def basic_parser(description='', **kwargs):
              'be handled by i.e. a Manager object.')
 
     return parser
+
+
+def config_parser(description='', **kwargs):
+    '''Returns a commandline parser for use with configuration files.
+
+    Returns an argparse.ArgumentParser set up for use with
+    configuration files.
+    '''
+
+    parser = argparse.ArgumentParser(description)
+    a = parser.add_argument('--lr', type=float,
+        help='Learning rate.')
+    a = parser.add_argument('--batch-size', type=int,
+        help='Batch size.')
+    a = parser.add_arguemtn('-e', '--epochs', type=int,
+        help='Number of epochs to train for.')
+    subparsers = parser.add_subparsers()
+
+    parse_start = subparsers.add_parser('start', aliases=['s'],
+        help='Start a new training run.')
+    parse_start.add_argument('config', help='Configuration file for the run.')
+
+    parse_cont = subparsers.add_parser('continue', aliases=['cont', 'c'],
+        help='Continue a completed or interrupted training run.')
+    a = parse_cont.add_argument('-i', '--rid', type=int, default=-1,
+        help='Integer representing the run id.')
+
+    return parser
+
---
+++ dnnutil/config.py
@@ -0,0 +1,58 @@
+import importlib
+import json
+from types import SimpleNamespace
+
+
+__all__ = ['configure']
+
+
+def configure(config_file):
+    '''TODO:docs
+    '''
+    cfg = _Configuration(config_file)
+    return cfg
+
+
+class _Configuration(object):
+    '''TODO:docs
+    '''
+
+    def __init__(self, config_file):
+        self.load(config_file)
+
+    def load(self, config_file):
+        cf = json.load(open(config_file))
+
+        # Network
+        model_def = cf['network']
+        model_mod = importlib.import_module(model_def['module'])
+        model_class = getattr(model_mod, model_def['model'])
+        model_kwargs = model_def['kwargs']
+
+        # Dataset
+        data_def = cf['dataset']
+        data_mod = importlib.import_module(data_def['module'])
+        dataset = getattr(data_mod, data_def['dataset'])
+        data_kwargs = data_def['kwargs']
+
+        # Loss
+        loss_def = cf['loss']
+        loss_mod = importlib.import_module(loss_def['module'])
+        loss = getattr(loss_mod, loss_def['loss'])
+        loss_kwargs = loss_def['kwargs']
+
+        # Hyperparameters
+        params  = cf['hyperparams']
+        
+        self.model = SimpleNamespace(model=model_class, args=model_kwargs)
+        self.data = SimpleNamespace(data=dataset, args=data_kwargs)
+        self.loss = SimpleNamespace(loss=loss, args=loss_kwargs)
+        self.hp = params
+
+    def __repr__(self):
+        s = 'Configuration{{\n{}\n}}'.format(
+            '\n'.join('{}: {!r}'.format(k, v) for k, v in self.__dict__.items()))
+        return s
+
+    def __str__(self):
+        return repr(self)
