363b45b8e680488091b69280abb8ea012f2c0211
Connor <catalys1@gmail.com>
2018-12-11 16:39:55 -0700

    Add custom parser to work with arguments passed from somewhere other than commandline.

--- dnnutil/cmdopts.py
+++ dnnutil/cmdopts.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+import sys
 
 
 __all__ = ['basic_parser', 'config_parser']
@@ -61,24 +62,54 @@ def config_parser(description='', **kwargs):
     Returns an argparse.ArgumentParser set up for use with
     configuration files.
     '''
-
-    parser = argparse.ArgumentParser(description)
+    rundir = kwargs.get('run_dir', 'runs/')
+    parser = _ConfigParser(description)
     a = parser.add_argument('--lr', type=float,
         help='Learning rate.')
     a = parser.add_argument('--batch-size', type=int,
         help='Batch size.')
-    a = parser.add_arguemtn('-e', '--epochs', type=int,
+    a = parser.add_argument('-e', '--epochs', type=int,
         help='Number of epochs to train for.')
+    a = parser.add_argument('--model', default='',
+        help='Path to model checkpoint')
+    a = parser.add_argument('-D', '--run-dir', default=rundir,
+        help='Path to root directory where runs should be saved.')
+    
     subparsers = parser.add_subparsers()
 
     parse_start = subparsers.add_parser('start', aliases=['s'],
-        help='Start a new training run.')
+        help='Start a new training run. Creates a new run directory and '
+             'begins training a model according to the provided config file.')
     parse_start.add_argument('config', help='Configuration file for the run.')
+    parse_start.set_defaults(setup='start', rid=0)
 
     parse_cont = subparsers.add_parser('continue', aliases=['cont', 'c'],
         help='Continue a completed or interrupted training run.')
-    a = parse_cont.add_argument('-i', '--rid', type=int, default=-1,
-        help='Integer representing the run id.')
+    a = parse_cont.add_argument('-i', '--rid', type=valid_continue_id,
+        default=-1,
+        help='Integer representing the run id. Use -1 for the most recently '
+             'created run, or a positive integer to select a different run. '
+             'Default: -1')
+    parse_cont.set_defaults(setup='continue')
 
     return parser
+
+
+def valid_continue_id(s):
+    val = int(s)
+    if val == 0 or val < -1:
+        raise argparse.ArgumentTypeError(
+            '{} is not a valid run id to continue from')
+    return val
+
+
+class _ConfigParser(argparse.ArgumentParser):
+    def parse_args(self, args=None, namespace=None):
+        if args is None:
+            args = sys.argv[1:]
+        if len(args) == 0:
+            self.print_help()
+            sys.exit(1)
+        ns = super(_ConfigParser, self).parse_args(args, namespace)
+        return ns
 
