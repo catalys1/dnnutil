dba96a60f1d3ab7351deb2b4d95de1f10158afa9
Connor <catalys1@gmail.com>
2018-12-11 16:52:24 -0700

    Updatate config.py

--- dnnutil/config.py
+++ dnnutil/config.py
@@ -16,7 +16,6 @@ def configure(config_file):
 class _Configuration(object):
     '''TODO:docs
     '''
-
     def __init__(self, config_file):
         self.load(config_file)
 
@@ -41,13 +40,24 @@ class _Configuration(object):
         loss = getattr(loss_mod, loss_def['loss'])
         loss_kwargs = loss_def['kwargs']
 
+        # Trainer
+        trainer_def = cf['trainer']
+        trainer_mod = importlib.import_module(trainer_def['module'])
+        trainer = getattr(trainer_mod, trainer_def['trainer'])
+
         # Hyperparameters
         params  = cf['hyperparams']
         
+        # Other
+        other = cf.get('other', {})
+
         self.model = SimpleNamespace(model=model_class, args=model_kwargs)
         self.data = SimpleNamespace(data=dataset, args=data_kwargs)
         self.loss = SimpleNamespace(loss=loss, args=loss_kwargs)
+        self.trainer = trainer
         self.hp = params
+        self.other = other
+
 
     def __repr__(self):
         s = 'Configuration{{\n{}\n}}'.format(
@@ -56,3 +66,4 @@ class _Configuration(object):
 
     def __str__(self):
         return repr(self)
+
